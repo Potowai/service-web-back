97cbbace35126fcec98993ebe037eed33066d8b7
Potowai <alexis.fiolleau@ecoles-epsi.net>
2024-05-31 17:49:30 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://tp-service-web.onrender.com', // l'URL de votre application React
+    methods: [],
+    credentials: true, // Si vous utilisez des cookies d'authentification
+  });
   await app.listen(3005);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
