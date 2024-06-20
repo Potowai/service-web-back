5a25375917b771dc064d15c3fa47011c700fcce7
Alexis <alexis.thomas.ines@gmail.com>
2024-06-20 22:26:49 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Utilisation de Helmet pour sécuriser les en-têtes HTTP
-
+  const config = new DocumentBuilder()
+  .setTitle('API MELODLE')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   // Ajout du middleware CORS
   app.enableCors({
     origin: [
       'https://melodle.netlify.app',
        // Ajoutez le domaine de votre frontend déployé
-       'https://melodle-one.vercel.app',
+      'https://melodle-one.vercel.app',
       'http://localhost:3000', // Pour le développement local
     ],
     methods: ['*'],
