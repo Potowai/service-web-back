cd2d7dbc5e61a14686ad77b8f7c228d348dbdf62
Alexis <alexis.thomas.ines@gmail.com>
2024-06-20 22:08:23 +0200

    try to fix vercel

--- src/main.ts
+++ src/main.ts
@@ -9,23 +9,14 @@ async function bootstrap() {
   // Ajout du middleware CORS
   app.enableCors({
     origin: [
-      'https://melodle.netlify.app', // Ajoutez le domaine de votre frontend déployé
+      'https://melodle.netlify.app',
+       // Ajoutez le domaine de votre frontend déployé
+       'https://melodle-one.vercel.app',
       'http://localhost:3000', // Pour le développement local
     ],
     methods: ['*'],
     allowedHeaders: ['*'],
     credentials: true,
-  });
-
-  // Ajout du middleware pour gérer les requêtes preflight OPTIONS
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://melodle.netlify.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(204);
-    }
-    next();
   });
 
   await app.listen(process.env.PORT || 3005);
